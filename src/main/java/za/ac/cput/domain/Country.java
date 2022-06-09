d2b616d80cff31f482879e46bd8c6a8117b936cf
ethan220126283 <220126283@mycput.ac.za>
2022-06-09 18:34:50 +0200

    Added Factory, Repository and Service Packages

--- src/main/java/za/ac/cput/domain/Country.java
+++ src/main/java/za/ac/cput/domain/Country.java
@@ -1,4 +1,15 @@
 package za.ac.cput.domain;
 
 public class Country {
+
+    private String id;
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/main/java/za/ac/cput/domain/Name.java
+++ src/main/java/za/ac/cput/domain/Name.java
@@ -2,4 +2,16 @@ package za.ac.cput.domain;
 
 public class Name {
 
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public Name() {
+    }
+
+    public Name(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
 }
