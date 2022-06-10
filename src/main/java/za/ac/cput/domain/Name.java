303e770d7169b159ccdf16ab2047c3733fb95cd3
ethan220126283 <220126283@mycput.ac.za>
2022-06-10 09:57:58 +0200

    Added the builder class for entity Name

--- src/main/java/za/ac/cput/domain/Name.java
+++ src/main/java/za/ac/cput/domain/Name.java
@@ -6,12 +6,82 @@ public class Name {
     private String middleName;
     private String lastName;
 
-    public Name() {
-    }
-
-    public Name(String firstName, String middleName, String lastName) {
+    //Private Constructors
+    private Name(Builder builder) {
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
+    }
+
+    private Name() {
+    }
+
+    //Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    //Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    //Builder Class
+    public static class Builder{
+
+        private String firstName;
+        private String middleName;
+        private String lastName;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder copy(Name name){
+            this.firstName = name.firstName;
+            this.middleName = name.middleName;
+            this.lastName = name.lastName;
+            return this;
+        }
+
+        public Name build(){
+            return new Name(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
