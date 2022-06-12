93fa8eab445a765fdbe58d1a7e95683e824f4845
ethan220126283 <220126283@mycput.ac.za>
2022-06-12 11:21:29 +0200

    Added Exception Test Case and removed invalid test cases

--- src/test/java/za/ac/cput/factory/NameFactoryTest.java
+++ src/test/java/za/ac/cput/factory/NameFactoryTest.java
@@ -1,6 +1,10 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.function.Executable;
 import za.ac.cput.domain.Name;
+
+import javax.management.InvalidApplicationException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,30 +22,19 @@ class NameFactoryTest {
     }
 
     @org.junit.jupiter.api.Test
-    void createNullName() {
-        Name name = NameFactory.createName(null,null,null);
-        System.out.println(name);
-        assertNull(name);
-    }
-
-    @org.junit.jupiter.api.Test
-    void createFirstName() {
-        Name name = NameFactory.createName("James",null,null);
-        System.out.println(name);
-        assertNull(name);
-    }
-
-    @org.junit.jupiter.api.Test
-    void createLastName() {
-        Name name = NameFactory.createName(null,null,"Smith");
-        System.out.println(name);
-        assertNull(name);
-    }
-
-    @org.junit.jupiter.api.Test
     void createFirstAndLastName() {
         Name name = NameFactory.createName("James",null,"Smith");
         System.out.println(name);
         assertNotNull(name);
+    }
+
+    @org.junit.jupiter.api.Test
+    void createFailName() {
+        IllegalArgumentException thrown = Assertions
+                .assertThrows(IllegalArgumentException.class, () -> {
+                    Name name = NameFactory.createName(null,null,null);
+                }, "IllegalArgumentException was expected");
+
+        assertEquals("firstName and lastName mandatory attributes.",thrown.getMessage());
     }
 }
