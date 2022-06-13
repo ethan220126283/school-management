8303cc8d7fe4ded662082c0f2c5f8ac7d2ae14bf
ethan220126283 <220126283@mycput.ac.za>
2022-06-13 11:29:58 +0200

    Commit

--- src/main/java/za/ac/cput/factory/NameFactory.java
+++ src/main/java/za/ac/cput/factory/NameFactory.java
@@ -1,5 +1,10 @@
 package za.ac.cput.factory;
 
+/* Student#: 220126283
+*  Name: Ethan Christopher Swart
+*  Subject: ADP3
+* */
+
 import za.ac.cput.domain.Name;
 import za.ac.cput.util.Helper;
 
@@ -8,10 +13,12 @@ public class NameFactory {
     //Factory method
     public static Name createName(String firstName, String middleName, String lastName){
 
+        //Check for null
         if (Helper.isNullorEmpty(firstName) || Helper.isNullorEmpty(lastName)){
             throw new IllegalArgumentException("firstName and lastName mandatory attributes.");
         }
 
+        //Return of Name entity
         return new Name.Builder(firstName)
                 .theirMiddleName(middleName)
                 .theirLastName(lastName)
--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,7 +1,13 @@
 package za.ac.cput.util;
 
+/* Student#: 220126283
+ *  Name: Ethan Christopher Swart
+ *  Subject: ADP3
+ * */
+
 public class Helper {
 
+    //Check for null or empty field
     public static boolean isNullorEmpty(String s){
         return (s == null || s.equals ("") || s.isEmpty()|| s.equalsIgnoreCase("null"));
     }
