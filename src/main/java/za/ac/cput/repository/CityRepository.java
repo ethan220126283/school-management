838e424efdd9f1eee2e5a015dc948a36457ff498
Tumelo Mzaca <206006330@mycput.ac.za>
2022-06-16 17:27:41 +0200

    Fourth Commit - add a new repository CityRepository find all

--- src/main/java/za/ac/cput/repository/CityRepository.java
+++ src/main/java/za/ac/cput/repository/CityRepository.java
@@ -4,6 +4,11 @@ import za.ac.cput.domain.City;
 
 import java.util.List;
 
+/**
+ * Working on findAll city
+ * Student Number - 206006330
+ */
+
 public interface CityRepository extends IRepository<City, String>{
 
     List<City> findAll();
