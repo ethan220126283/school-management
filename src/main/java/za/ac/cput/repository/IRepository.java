e7037b63cf2f2daa71da390ba74dd4970af98d51
Tumelo Mzaca <206006330@mycput.ac.za>
2022-06-16 17:33:38 +0200

    Sixth Commit - add a new repository IRepository test for CRUD

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -2,6 +2,13 @@ package za.ac.cput.repository;
 
 import java.util.Optional;
 
+/**
+ * Working on Test CRUD
+ * Student Number - 206006330
+ * @param <T>
+ * @param <ID>
+ */
+
 public interface IRepository <T, ID>{
 
     T save(T t);
