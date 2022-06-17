6fb9117959ff76e728f8a0990f6319b1ff79be81
ethan220126283 <220126283@mycput.ac.za>
2022-06-17 14:17:42 +0200

    Cleaned messy code

--- src/main/java/za/ac/cput/domain/Student.java
+++ src/main/java/za/ac/cput/domain/Student.java
@@ -10,9 +10,7 @@ import java.io.Serializable;
 @Entity
 public class Student implements Serializable {
 
-    @NotNull
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String studentId;
 
     private String email;
--- src/main/java/za/ac/cput/service/impl/StudentServiceImpl.java
+++ src/main/java/za/ac/cput/service/impl/StudentServiceImpl.java
@@ -1,6 +1,8 @@
 //220126283
 package za.ac.cput.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import za.ac.cput.domain.Student;
 import za.ac.cput.repository.StudentRepository;
 import za.ac.cput.service.StudentService;
@@ -9,11 +11,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository repository;
 
-    private StudentServiceImpl(StudentRepository repository) {
+    @Autowired
+    public StudentServiceImpl(StudentRepository repository) {
         this.repository = repository;
     }
 
@@ -31,5 +35,14 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public void delete(Student student) {
         this.repository.delete(student);
+    }
+
+    @Override
+    public List<Student> findAll() { return this.repository.findAll(); }
+
+    @Override
+    public void deleteById(String id) {
+        Optional<Student> student = read(id);
+        if (student.isPresent()) delete(student.get());
     }
 }
