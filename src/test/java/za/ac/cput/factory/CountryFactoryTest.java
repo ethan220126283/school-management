ff584f2adef455f0c1bd32131226041b6b1c864a
Darryll Merkeur <darryll.merkeur@gmail.com>
2022-06-17 14:18:45 +0200

    updated country

--- src/test/java/za/ac/cput/factory/CountryFactoryTest.java
+++ src/test/java/za/ac/cput/factory/CountryFactoryTest.java
@@ -1,28 +1,22 @@
 package za.ac.cput.factory;
-import org.junit.jupiter.api.Assertions;
-/*
-        Student: Darryll Merkeur (220253595)
-        Date: 18 June 2022
-*/
+import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Country;
 import static org.junit.jupiter.api.Assertions.*;
-
 class CountryFactoryTest {
-
-    @org.junit.jupiter.api.Test
-    public void createCountry(){
-        Country country = CountryFactory.createCountry("Kongo","KONG456");
-        assertNotNull(country);
+    @Test
+    public void buildCountry(){
+        Country country= CountryFactory.build
+                ("STUDE12365","South Africa");
         System.out.println(country);
-    }
-    @org.junit.jupiter.api.Test
-    void createId(){
-        Country country = CountryFactory.createCountry("South Africa","STUD12365");
         assertNotNull(country);
-        System.out.println(country);
-
-
     }
-
-
+    @Test void notPassed(){
+        Exception exception= assertThrows
+                ((IllegalArgumentException.class),()->CountryFactory.build(null,"South Africa"));
+        String exceptionMessage = exception.getMessage();
+        System.out.println
+                (exceptionMessage);
+        assertNotSame
+                ("please insert countryID", exceptionMessage);
+    }
 }
