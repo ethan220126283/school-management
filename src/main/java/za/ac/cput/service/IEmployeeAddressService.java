56dba376f580e8bc8e22aa152fe946f823cf9441
lana-africa <216166640@mycput.ac.za>
2022-06-18 04:18:33 +0200

    Employee Address Service

--- src/main/java/za/ac/cput/service/IEmployeeAddressService.java
+++ src/main/java/za/ac/cput/service/IEmployeeAddressService.java
@@ -1,7 +1,13 @@
 package za.ac.cput.service;
-
+/*
+    IEmployeeAddressService.java
+    IEmployee Address Service
+    Lana Africa (216166640)
+*/
 import za.ac.cput.domain.EmployeeAddress;
+import java.util.List;
 
 public interface IEmployeeAddressService extends IService<EmployeeAddress, String> {
-
+List<EmployeeAddress> findAll();
+    void delete(String id);
 }
